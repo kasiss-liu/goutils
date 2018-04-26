64cf34c382b4ec5591f63d63c9ec0932286356b3
kasiss <xueyezhisahng@163.com>
2018-04-27 00:37:16 +0800

    增加测试用例

--- sessions/session_test.go
+++ sessions/session_test.go
@@ -45,3 +45,37 @@ func TestMemSessions(t *testing.T) {
 	DelSession(resp, newSession)
 
 }
+
+func TestFileSessions(t *testing.T) {
+	store := NewFileSessionStorage("./", "sess_")
+	initMem(store)
+	CunstomSessionStorage(store)
+	SetCookieSessionName("TEST_SESSION")
+	newSession := NewSession("/", "localhost", 300, false, false)
+
+	getValue := newSession.Get("test")
+	fmt.Println("val:", getValue)
+
+	l := newSession.Len()
+	fmt.Println("len:", l)
+
+	newSession.Del("test")
+
+	l = newSession.Len()
+	fmt.Println("len:", l)
+
+	isGc := newSession.GC()
+	fmt.Println("Gc:", isGc)
+
+	resp := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "localhost:8999", nil)
+	newSession.Save(resp, req)
+	sess, err := GetSession(req)
+	if err != nil {
+		fmt.Println("getSess:", sess)
+	} else {
+		t.Error(err.Error())
+	}
+
+	DelSession(resp, newSession)
+}
